21e89d7571871fa108f5c9b191d7294a4ece2d37
Joshua <78288453+friedglue@users.noreply.github.com>
2021-01-30 23:00:42 -0500

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,19 @@
+/*********************
+* Hello
+**********************
+* Joshua Hayes
+* 1/30/21
+* CMSC-225-003
+**********************/
+
+public class Hello {
+	public static void main(String[] args) {
+
+		System.out.println("Joshua Hayes");
+		System.out.println("Hello");
+		System.out.println("Introduction Lab 1");
+		System.out.println("Version 1 - 01.30.20");
+		System.out.println("CMSC 225 003");
+		System.out.println();
+	}
+}
